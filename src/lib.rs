d4c868c1d7ccd18833a45c2f77ae2f36d9b181da
Jimmy Bergström <me@kwarf.com>
2021-04-23 22:36:50 +0200

    get_track_value_smooth

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,62 @@ impl SyncDevice {
             return Err(SyncError::TrackDoesntExist);
         }
     }
+
+    pub fn get_track_value_smooth(&self, track_id: usize) -> Result<f64, SyncError> {
+        if self.tracks.len() > track_id {
+            let track = &self.tracks[track_id];
+            let row = self.get_row_from_time();
+
+            let hit_idx: usize;
+            if let Some(idx) = track.find_active_key_idx_for_row(row) {
+                use self::ActiveKeyIdx::*;
+                match idx {
+                    ActiveKeyIdx::ExactRow(n) | ActiveKeyIdx::PrevRow(n) => {
+                        if n == track.keys.len() - 1 {
+                            return Ok(track.keys[track.keys.len() - 1].value as f64);
+                        }
+                        hit_idx = n
+                    },
+                    AfterLastRow => return Ok(track.keys[track.keys.len() - 1].value as f64),
+                    BeforeFirstRow => return Ok(track.keys[0].value as f64),
+                }
+            } else {
+                return Ok(0.0);
+            }
+
+            let cur_key = &track.keys[hit_idx];
+            // Return early for Step keys
+            match cur_key.key_type {
+                KeyType::Step => return Ok(cur_key.value as f64),
+                _ => {}
+            }
+
+            // Calculate interpolated keys
+            let next_key = &track.keys[hit_idx + 1];
+            let cur_key_time = self.get_time_from_key(cur_key) as f64;
+            let next_key_time = self.get_time_from_key(next_key) as f64;
+            let t = (self.time as f64 - cur_key_time) / (next_key_time - cur_key_time);
+            let a = cur_key.value as f64;
+            let b = (next_key.value - cur_key.value) as f64;
+
+            Ok(match cur_key.key_type {
+                KeyType::Linear => a + b * t,
+                KeyType::Smooth => a + b * (t * t * (3.0 - 2.0 * t)),
+                KeyType::Ramp => a + b * t * t,
+                _ => 0.0,
+            })
+        } else {
+            Err(SyncError::TrackDoesntExist)
+        }
+    }
+
+    fn get_row_from_time(&self) -> u32 {
+        ((self.time as f64 / 1000.0) * self.rps) as u32
+    }
+
+    fn get_time_from_key(&self, key: &TrackKey) -> u32 {
+        (key.row as f64 / self.rps * 1000.0) as u32
+    }
 }
 
 pub struct SyncTrack {
@@ -238,5 +294,116 @@ pub fn code_to_key(code: u8) -> KeyType {
         2 => Smooth,
         3 => Ramp,
         _ => NOOP,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_track(interpolation: u8) -> SyncTrack {
+        let mut track = SyncTrack::new();
+
+        track.add_key(TrackKey {
+            row: 0,
+            value: 0f32,
+            key_type: code_to_key(interpolation),
+        });
+
+        track.add_key(TrackKey {
+            row: 3,
+            value: 1f32,
+            key_type: code_to_key(interpolation),
+        });
+
+        track
+    }
+
+    #[test]
+    fn test_can_get_track_value() {
+        let mut device = SyncDevice::new(120.0, 4);
+        device
+            .tracks
+            .push(setup_track(key_to_code(&KeyType::Linear)));
+
+        device.row = 0;
+        assert_eq!(0.0, device.get_track_value(0).unwrap_or(-1.0));
+
+        device.row = 1;
+        assert_eq!(
+            0.3333333333333333,
+            device.get_track_value(0).unwrap_or(-1.0)
+        );
+
+        device.row = 2;
+        assert_eq!(
+            0.6666666666666666,
+            device.get_track_value(0).unwrap_or(-1.0)
+        );
+
+        device.row = 3;
+        assert_eq!(1.0, device.get_track_value(0).unwrap_or(-1.0));
+    }
+
+    #[test]
+    fn test_can_get_track_value_smooth() {
+        let mut device = SyncDevice::new(120.0, 4);
+        device
+            .tracks
+            .push(setup_track(key_to_code(&KeyType::Linear)));
+
+        // At 120 BPM we have 0.5s per beat, and 4 rows per beat gives 0.125s per row.
+        // So the values at 0, 0.125, 0.25 and 0.375 should line up with the values for the rows.
+        device.time = 0;
+        assert_eq!(
+            0.0,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+        device.time = 125;
+        assert_eq!(
+            0.3333333333333333,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+        device.time = 250;
+        assert_eq!(
+            0.6666666666666666,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+        device.time = 375;
+        assert_eq!(
+            1.0,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+
+        // But now we should be able to interpolate between rows as well..
+        device.time = 63;
+        assert_eq!(
+            0.168,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+        device.time = 188;
+        assert_eq!(
+            0.5013333333333333,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
+        device.time = 312;
+        assert_eq!(
+            0.832,
+            device
+                .get_track_value_smooth(0)
+                .unwrap_or(-1.0)
+        );
     }
 }
